17239f6f5ab01e9c0a2460634c60a16dd3aa8ee2
Anthony Tonatihu Rosales Ceja <arosalesceja1997@gmail.com>
2020-04-22 21:40:06 -0500

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,52 @@
+let Hanilidades = 
+[
+    "Html", "JavaScript", "Css",
+    "NodeJs", "VueJs", "ReactJs",
+    "React Native", "C++", "C#",
+    "PHP", "Laravel", "MongoDB",
+    "Postgresql", "MySQL", "SQL",
+    "Java", "Spring Boot"
+];
+
+let Valor = 
+[
+    4, 4, 3,
+    3, 4, 3,
+    2, 3, 3,
+    3, 3, 2,
+    2, 4, 3,
+    3, 1
+]
+
+// let tabla = document.getElementById();
+agregarFila();
+function agregarFila(){
+    let x = "";
+    for(let i = 0;  i < Hanilidades.length; i++) {
+        x = "cam"+i;
+        document.getElementById("table-id").insertRow(-1).innerHTML = '<tr><td>'+ Hanilidades[i] +'</td>'
+        +'<td id="cam'+i+'"><div class="ajustar"></div></td></tr>';
+        let miVal = agregarValor(x, Valor[i]);
+    }
+}
+
+function agregarValor(idTr, cant){
+    //contenedor div y asignacion del atributo
+    for(let i = 0;  i < cant; i++) {
+        let divcontenedora = document.createElement("div");
+        let atributo= document.createAttribute("class");
+        atributo.value="circulo";
+        divcontenedora.setAttributeNode(atributo);
+        let miVal1 = document.getElementById(idTr);
+        miVal1.appendChild(divcontenedora);
+    }
+
+    for(let i = 0;  i < 6-cant; i++) {
+        let divcontenedora = document.createElement("div");
+        let atributo= document.createAttribute("class");
+        atributo.value="circulo-vacio";
+        divcontenedora.setAttributeNode(atributo);
+        let miVal1 = document.getElementById(idTr);
+        miVal1.appendChild(divcontenedora);
+    }
+}
